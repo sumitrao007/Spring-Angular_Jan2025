cb852e4a06b68874f4ddf91999669531c10b9089
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-01-16 10:54:25 +0530

    Day09

---
+++ Day09/DemoIOC/src/main/java/com/hefshine/entity/Student.java
@@ -0,0 +1,34 @@
+package com.hefshine.entity;
+
+public class Student {
+	
+	private Integer id;
+	private String name;
+	
+	public Student() {}
+	
+	public Student(String name) {
+		super();
+		this.name = name;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+
+}
