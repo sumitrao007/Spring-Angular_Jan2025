67eddce3f540b62739666cb4b839ee35e8c50c55
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-02-04 09:57:25 +0530

    Day22

---
+++ Day22/propertybinding/propertybinding.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propertybinding',
+  templateUrl: './propertybinding.component.html',
+  styleUrls: ['./propertybinding.component.css']
+})
+export class PropertybindingComponent {
+
+  name:string='Sumit';
+  isHidden:boolean=false;
+  
+}
