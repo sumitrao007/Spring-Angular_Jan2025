af7b3ae8ac7f84a129b389f34c9134be22a7a733
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-02-19 14:29:22 +0530

    Day32

---
+++ Day32/signup/signup.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+  onSubmit(signupForm:NgForm){
+
+  }
+
+}
